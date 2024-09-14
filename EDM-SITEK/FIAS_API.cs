e89081718ab497fceeb423fd8dfa4cd53b183b41
strifeslider <green1elte@gmail.com>
2024-09-14 18:33:34 +0400

    Integred FIAS_API

--- EDM-SITEK/FIAS_API.cs
+++ EDM-SITEK/FIAS_API.cs
@@ -1,9 +1,21 @@
-﻿namespace EDM_SITEK
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EDM_SITEK
 {
     public class FIAS_API
     {
-        string FIAS_Url = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
-        FIAS_Url_Data data = new FIAS_Url_Data();
+        static string FIAS_Url = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
+        string Tmp_Dir = "//Tmp_Dir//";
 
+        static HttpClient httpClient = new  HttpClient();
+        public async Task<FIAS_Url_Data> API_Call() {
+            HttpResponseMessage response = (await httpClient.GetAsync(FIAS_Url)).EnsureSuccessStatusCode();
+            var ResponseBody = await response.Content.ReadAsStringAsync();
+            FIAS_Url_Data data = JsonConvert.DeserializeObject<FIAS_Url_Data>(ResponseBody);
+            return data;
+        }
     }
 }

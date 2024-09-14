dedf38c08f0e6ccdc9402d06d284ff28a92f0888
strifeslider <green1elte@gmail.com>
2024-09-14 19:38:40 +0400

    FIAS_API

--- EDM-SITEK/FIAS_API.cs
+++ EDM-SITEK/FIAS_API.cs
@@ -1,21 +1,32 @@
 ﻿using Newtonsoft.Json;
-using System;
 using System.Net;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace EDM_SITEK
 {
     public class FIAS_API
     {
-        static string FIAS_Url = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
+        string FIAS_Url = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
         string Tmp_Dir = "//Tmp_Dir//";
 
-        static HttpClient httpClient = new  HttpClient();
-        public async Task<FIAS_Url_Data> API_Call() {
-            HttpResponseMessage response = (await httpClient.GetAsync(FIAS_Url)).EnsureSuccessStatusCode();
-            var ResponseBody = await response.Content.ReadAsStringAsync();
-            FIAS_Url_Data data = JsonConvert.DeserializeObject<FIAS_Url_Data>(ResponseBody);
-            return data;
+        HttpClient httpClient = new  HttpClient();
+
+        WebClient webClient = new WebClient();
+        public void init()
+        {
+            async Task<FIAS_Url_Data> API_Call()
+            {
+                HttpResponseMessage response = (await httpClient.GetAsync(FIAS_Url)).EnsureSuccessStatusCode();
+                var ResponseBody = await response.Content.ReadAsStringAsync();
+                FIAS_Url_Data data = JsonConvert.DeserializeObject<FIAS_Url_Data>(ResponseBody);
+                return data;
+            }
+            async Task Delta_Data_Download()
+            {
+                FIAS_Url_Data data = await API_Call();
+                webClient.DownloadFile(data.GarXMLDeltaURL, Tmp_Dir + "//" + data.Date);
+            }
         }
+        
+        
     }
 }

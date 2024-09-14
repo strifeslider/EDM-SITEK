0ead90dd2fc62f47121be29c923494fd768e9a1c
strifeslider <green1elte@gmail.com>
2024-09-14 14:00:57 +0400

    Init Fias API and data model

---
+++ EDM-SITEK/FIAS_API.cs
@@ -0,0 +1,9 @@
+﻿namespace EDM_SITEK
+{
+    public class FIAS_API
+    {
+        string FIAS_Url = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
+        FIAS_Url_Data data = new FIAS_Url_Data();
+
+    }
+}
---
+++ EDM-SITEK/FIAS_Data.cs
@@ -0,0 +1,18 @@
+﻿namespace EDM_SITEK
+{
+    public class FIAS_Url_Data
+    {
+        public int VersionId { get; set; }
+        public string TextVersion { get; set; }
+        public string FiasCompleteDbfUrl { get; set; }
+        public string FiasCompleteXmlUrl { get; set; }
+        public string FiasDeltaDbfUrl { get; set; }
+        public string FiasDeltaXmlUrl { get; set; }
+        public string Kladr4ArjUrl { get; set; }
+        public string Kladr47ZUrl { get; set; }
+        public string GarXMLFullURL { get; set; }
+        public string GarXMLDeltaURL { get; set; }
+        public DateTime ExpDate { get; set; }
+        public string Date { get; set; }
+    }
+}

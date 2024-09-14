d2ce57283e3dc9ad66fcd73f0c1f3471bdfda8bd
strifeslider <green1elte@gmail.com>
2024-09-14 23:20:42 +0400

    Fixed Downloading

--- EDM-SITEK/FIAS_API.cs
+++ EDM-SITEK/FIAS_API.cs
@@ -1,32 +1,37 @@
-﻿using Newtonsoft.Json;
+﻿
+using Newtonsoft.Json;
+using System.IO;
 using System.Net;
+using System.Runtime;
 
 namespace EDM_SITEK
 {
     public class FIAS_API
     {
-        string FIAS_Url = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
-        string Tmp_Dir = "//Tmp_Dir//";
 
-        HttpClient httpClient = new  HttpClient();
 
+
+        HttpClient httpClient = new HttpClient();
         WebClient webClient = new WebClient();
-        public void init()
+        string FIAS_Url = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
+        string Tmp_Dir = "Tmp_Dir/";
+        async Task<FIAS_Url_Data> API_Call()
         {
-            async Task<FIAS_Url_Data> API_Call()
-            {
-                HttpResponseMessage response = (await httpClient.GetAsync(FIAS_Url)).EnsureSuccessStatusCode();
-                var ResponseBody = await response.Content.ReadAsStringAsync();
-                FIAS_Url_Data data = JsonConvert.DeserializeObject<FIAS_Url_Data>(ResponseBody);
-                return data;
-            }
-            async Task Delta_Data_Download()
-            {
-                FIAS_Url_Data data = await API_Call();
-                webClient.DownloadFile(data.GarXMLDeltaURL, Tmp_Dir + "//" + data.Date);
-            }
+            HttpResponseMessage response = (await httpClient.GetAsync(FIAS_Url)).EnsureSuccessStatusCode();
+            var ResponseBody = await response.Content.ReadAsStringAsync();
+            FIAS_Url_Data data = JsonConvert.DeserializeObject<FIAS_Url_Data>(ResponseBody);
+            
+            return data;
         }
-        
-        
+        public async Task Delta_Data_Download()
+        {
+
+            FIAS_Url_Data data = await API_Call();
+            DirectoryInfo dirInfo = new DirectoryInfo(Tmp_Dir);
+            
+            dirInfo.CreateSubdirectory(data.Date);
+            
+            webClient.DownloadFile(data.GarXMLDeltaURL, Tmp_Dir + data.Date+ ".zip");
+        }
     }
 }
--- EDM-SITEK/Program.cs
+++ EDM-SITEK/Program.cs
@@ -3,7 +3,9 @@ using EDM_SITEK;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 FIAS_API fiasapi = new FIAS_API();
-fiasapi.init();
-app.Run( 
-    
+
+app.MapGet("/", () => "Hello World!");
+string FIAS_Url = "https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo";
+await fiasapi.Delta_Data_Download();
+app.Run(
     );

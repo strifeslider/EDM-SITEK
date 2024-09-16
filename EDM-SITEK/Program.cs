177551e6308f0baf85965feea96bfabf416d3c5a
strifeslider <green1elte@gmail.com>
2024-09-16 19:48:34 +0400

    Finished WebForm output. Start testing

--- EDM-SITEK/Program.cs
+++ EDM-SITEK/Program.cs
@@ -18,13 +18,135 @@ app.Run(async (context) =>
         context.Response.ContentType = "text/html; charset=utf-8";
 
 
-        var stringBuilder = new System.Text.StringBuilder("<h3>Отчет по Обновлениям/h3><table>");
-        var obj = db.data.ToList();
+        var stringBuilder = new System.Text.StringBuilder("<h3>Отчет по Обновлениям Субьектов РФ</h3><table>");
+        List<OBJECT> obj = db.data.ToList();
         foreach(OBJECT objct in obj)
         {
-           
+            if (objct.LEVEL == 1)
+            {
                 stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
-           
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Административных районов</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 2)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Муниципальных районов</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 3)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Сельских/Городских поселений</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 4)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Городов</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 5)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Населенных пунктов</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 6)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Элементов планировочной структуры</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 7)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Элементов улично-дорожных сетей</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 8)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Зданий(строений), сооружений</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 10)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Помещений</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 11)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
+        }
+        stringBuilder.Append("</table>");
+
+        stringBuilder.Append('\n');
+        stringBuilder.Append("<h3>Отчет по Обновлениям Помещений в пределах помещений</h3><table>");
+        foreach (OBJECT objct in obj)
+        {
+            if (objct.LEVEL == 12)
+            {
+                stringBuilder.Append($"<tr><td>{objct.TYPENAME}</td><td>{objct.NAME}</td></tr>");
+                obj.Remove(objct);
+            }
         }
         stringBuilder.Append("</table>");
 
